00bd19a3f22cb5783f998aa0182b4adead236ca4
Sumudu Lansakara <sumudu.lansakara@gmail.com>
2018-03-30 17:33:00 +0200

    added player module for performing challenges
    Signed-off-by: Sumudu Lansakara <sumudu.lansakara@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import sys
 
-from selenium.common import exceptions
-
-import brain
 import exercise_page
 import home_page
 import lesson_page
 import login_page
+import player
 import translator
 import webdriver
 
@@ -33,7 +31,7 @@ def start(user, pwd):
     exercise_page.init_exercise("Basics-1")
     exercise_page.click_lesson("1")
 
-    challenges = do_exercise_2()
+    challenges = player.do_all_challenges()
     print("{} challenges completed".format(challenges))
 
     # exercise complete
@@ -41,79 +39,6 @@ def start(user, pwd):
     lesson_page.click_next()
 
     webdriver.close()
-
-
-def do_exercise_2():
-    cid = 0
-    prev_header = None
-    prev_data = 0
-    MAX_LOAD_ATTEMPTS = 10
-    while True:
-        try:
-            challenge_header = lesson_page.get_challenge_header().strip()
-            challenge_load_attempts = 0
-            while challenge_load_attempts < MAX_LOAD_ATTEMPTS:
-                challenge_load_attempts += 1
-                if challenge_header:
-                    if challenge_header != prev_header:
-                        break
-                    type_, _ = brain.decide_type_and_data(challenge_header)
-                    if type_ == brain.ChallengeType.write_in_english:
-                        hint = lesson_page.get_hint_sentence(challenge_load_attempts / 2)
-                        hint = translator.sanitize(hint)
-                        if hint != prev_data:
-                            break
-                challenge_header = lesson_page.get_challenge_header().strip()
-            else:
-                print("### max attempts reached")
-                return cid
-        except exceptions.StaleElementReferenceException:
-            print("### StaleElementReferenceException")
-            return cid
-        except exceptions.TimeoutException:
-            print("### TimeoutException")
-            return cid
-        except Exception:
-            print("### Generic")
-            return cid
-        cid += 1
-        type_, data = brain.decide_type_and_data(challenge_header)
-        if type_ == brain.ChallengeType.write_in_english:
-            hint = lesson_page.get_hint_sentence()
-            hint = translator.sanitize(hint)
-            print("challenge {}: {} [{}]".format(cid, challenge_header, hint))
-            prev_data = hint
-            lesson_page.type_english(translator.g2e(hint))
-        else:
-            print("challenge {}: {}".format(cid, challenge_header))
-            prev_data = data
-            if type_ == brain.ChallengeType.write_in_german:
-                lesson_page.type_german(translator.e2g(data))
-            elif type_ == brain.ChallengeType.select_in_german:
-                lesson_page.select_radio(translator.e2g(data))
-        lesson_page.click_next()
-        lesson_page.click_next()
-        lesson_page.wait_till_header_changed(challenge_header, 1)
-        prev_header = challenge_header
-
-
-def do_exercise_1():
-    # Lesson page: do challenges
-    for i in range(7):
-        challenge = lesson_page.get_challenge_header()
-        print("challenge {}: {}".format(i + 1, challenge))
-        type_, data = brain.decide_type_and_data(challenge)
-        if type_ == brain.ChallengeType.write_in_english:
-            hint = lesson_page.get_hint_sentence()
-            lesson_page.type_english(translator.g2e(hint))
-        elif type_ == brain.ChallengeType.write_in_german:
-            lesson_page.type_german(translator.e2g(data))
-        elif type_ == brain.ChallengeType.select_in_german:
-            lesson_page.select_radio(translator.e2g(data))
-        lesson_page.click_next()
-        lesson_page.click_next()
-        lesson_page.wait_till_header_changed(challenge, 1)
-        # time.sleep(0.4)
 
 
 def get_user_credentials():
---
+++ src/player.py
@@ -0,0 +1,88 @@
+import re
+from enum import Enum
+
+from selenium.common import exceptions
+
+import lesson_page
+import translator
+
+MAX_LOAD_ATTEMPTS = 10
+
+
+def do_all_challenges():
+    cid = 0
+    prev_header = None
+    prev_data = 0
+    while True:
+        try:
+            challenge_header = lesson_page.get_challenge_header().strip()
+            challenge_load_attempts = 0
+            while challenge_load_attempts < MAX_LOAD_ATTEMPTS:
+                challenge_load_attempts += 1
+                if challenge_header:
+                    if challenge_header != prev_header:
+                        break
+                    type_, _ = _decide_type_and_data(challenge_header)
+                    if type_ == ChallengeType.write_in_english:
+                        hint = lesson_page.get_hint_sentence(challenge_load_attempts / 2)
+                        hint = translator.sanitize(hint)
+                        if hint != prev_data:
+                            break
+                challenge_header = lesson_page.get_challenge_header().strip()
+            else:
+                print("### max attempts reached")
+                return cid
+        except exceptions.StaleElementReferenceException:
+            print("### StaleElementReferenceException")
+            return cid
+        except exceptions.TimeoutException:
+            print("### TimeoutException")
+            return cid
+        except Exception:
+            print("### Generic")
+            return cid
+        cid += 1
+        type_, data = _decide_type_and_data(challenge_header)
+        if type_ == ChallengeType.write_in_english:
+            hint = lesson_page.get_hint_sentence()
+            hint = translator.sanitize(hint)
+            print("challenge {}: {} [{}]".format(cid, challenge_header, hint))
+            prev_data = hint
+            lesson_page.type_english(translator.g2e(hint))
+        else:
+            print("challenge {}: {}".format(cid, challenge_header))
+            prev_data = data
+            if type_ == ChallengeType.write_in_german:
+                lesson_page.type_german(translator.e2g(data))
+            elif type_ == ChallengeType.select_in_german:
+                lesson_page.select_radio(translator.e2g(data))
+        lesson_page.click_next()
+        lesson_page.click_next()
+        lesson_page.wait_till_header_changed(challenge_header, 1)
+        prev_header = challenge_header
+
+
+class ChallengeType(Enum):
+    write_in_english = 1
+    write_in_german = 2
+    select_in_german = 3
+
+
+def _decide_type_and_data(challenge):
+    """ return challenge type and extracted data required for the solution """
+    challenge = challenge.lower()
+
+    # write in English
+    if challenge == "write this in english":
+        return ChallengeType.write_in_english, None
+
+    # write in German
+    wig_regex = re.match(r'write .(.*). in german', challenge)
+    if wig_regex:
+        return ChallengeType.write_in_german, wig_regex.group(1)
+
+    # select in German
+    sig_regex = re.match(r'select the word for .(.*).', challenge)
+    if sig_regex:
+        return ChallengeType.select_in_german, sig_regex.group(1)
+    return None, None
